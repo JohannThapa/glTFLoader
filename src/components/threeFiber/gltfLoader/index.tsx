e4f43774dc907b2adf570b13461e7e3a85602f49
JohannThapa <rupakmagar182013.rm@gmail.com>
2022-09-27 23:44:05 +0545

    style: MUI layout

---
+++ src/components/threeFiber/gltfLoader/index.tsx
@@ -0,0 +1,16 @@
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import { Canvas, useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const GltfLoaderFiber = () => {
+
+return (
+    <>
+     test
+   </>
+)
+}
+
+export default GltfLoaderFiber;
